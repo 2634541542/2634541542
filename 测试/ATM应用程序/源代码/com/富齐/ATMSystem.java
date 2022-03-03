6975f0572a62305f7782777177b472492bade8d9
张扬 <92300416+2634541542@users.noreply.github.com>
2022-03-04 00:06:04 +0800

    Update and rename testing/ATM-app/src/com/hfutzy/ATMSystem.java to 测试/ATM应用程序/源代码/com/富齐/ATMSystem.java

--- 测试/ATM应用程序/源代码/com/富齐/ATMSystem.java
+++ 测试/ATM应用程序/源代码/com/富齐/ATMSystem.java
@@ -6,64 +6,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ATMSystem {
-    public class Account {
-        private String cardId;    //卡号
-        private String userName;  //用户名
-        private String passWord;  //密码
-        private double money;     //余额
-        private double quotaMoney;//档次取现额度
-
-        public Account() {
-        }
-
-        public Account(String cardId, String userName, String passWord, double quotaMoney) {
-            this.cardId = cardId;
-            this.userName = userName;
-            this.passWord = passWord;
-            this.quotaMoney = quotaMoney;
-        }
-
-        public String getCardId() {
-            return cardId;
-        }
-
-        public void setCardId(String cardId) {
-            this.cardId = cardId;
-        }
-
-        public String getUserName() {
-            return userName;
-        }
-
-        public void setUserName(String userName) {
-            this.userName = userName;
-        }
-
-        public String getPassWord() {
-            return passWord;
-        }
-
-        public void setPassWord(String passWord) {
-            this.passWord = passWord;
-        }
-
-        public double getMoney() {
-            return money;
-        }
-
-        public void setMoney(double money) {
-            this.money = money;
-        }
-
-        public double getQuotaMoney() {
-            return quotaMoney;
-        }
-
-        public void setQuotaMoney(double quotaMoney) {
-            this.quotaMoney = quotaMoney;
-        }
-    }
-
     public static void main(String[] args) {
         //定义集合，用于存储账户对象
         ArrayList<Account> accounts = new ArrayList<>();
@@ -420,9 +362,6 @@ public class ATMSystem {
                 System.out.println("密码错误");
             }
         }
-
     }
-
-
 }
 
